532918572eb281a4a7ea788d8306404997c539bb
alvarocalace <alvicala_10@hotmail.com>
2015-04-23 01:29:52 -0300

    generar tarea repetidos

--- TPO Aplicaciones Distribuidas/src/model/impl/MantenimientoEspecifico.java
+++ TPO Aplicaciones Distribuidas/src/model/impl/MantenimientoEspecifico.java
@@ -1,7 +1,5 @@
 package model.impl;
 
-import java.util.Calendar;
-import java.util.Date;
 
 
 public class MantenimientoEspecifico extends EstrategiaMantenimiento {
@@ -10,17 +8,6 @@ public class MantenimientoEspecifico extends EstrategiaMantenimiento {
 	protected void mantener() {
 		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	protected Tarea generarTarea(PlanMantenimiento plan) {
-		Calendar cal = Calendar.getInstance();
-		Date entrega = cal.getTime();
-		
-		cal.add(Calendar.DATE, 1);
-		Date devolucion = cal.getTime();
-		
-		return new Tarea(plan.kilometraje, entrega, devolucion);
 	}
 
 }

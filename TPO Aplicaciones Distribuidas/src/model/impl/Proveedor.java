6d41f1bf743e7cce2661c35e0d427b0c6276a14e
alvarocalace <alvicala_10@hotmail.com>
2015-04-23 01:35:31 -0300

    provee2

--- TPO Aplicaciones Distribuidas/src/model/impl/Proveedor.java
+++ TPO Aplicaciones Distribuidas/src/model/impl/Proveedor.java
@@ -3,5 +3,24 @@ package model.impl;
 import java.io.Serializable;
 
 public class Proveedor implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4550038024972428465L;
 
+	private String cuit;
+	private String nombre;
+	
+	public String getCuit() {
+		return cuit;
+	}
+	public void setCuit(String cuit) {
+		this.cuit = cuit;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}	
 }
